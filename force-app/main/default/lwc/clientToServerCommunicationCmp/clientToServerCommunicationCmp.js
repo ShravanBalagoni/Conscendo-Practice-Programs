bdf65f66f2027b5c77a42c4518a05e6a6592902b
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-27 15:20:35 +0530

    Modified errors

--- force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
+++ force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
@@ -1,10 +1,19 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import getPersonName from '@salesforce/apex/SampleApexLtngWebCmpnt.getPersonName';
 import getContacts from '@salesforce/apex/SampleApexLtngWebCmpnt.getContacts';
 
-
 export default class ClientToServerCommunicationCmp extends LightningElement {
-    @wire (getPersonName) personName;
+
+    @wire(getPersonName) personName;
+
     listOfContacts;
-    @wire(getContacts) contacts;
+
+    @wire(getContacts)
+    wiredContacts({ error, data }) {
+        if (data) {
+            this.listOfContacts = data;
+        } else if (error) {
+            console.error('Error: ' + JSON.stringify(error));
+        }
+    }
 }
