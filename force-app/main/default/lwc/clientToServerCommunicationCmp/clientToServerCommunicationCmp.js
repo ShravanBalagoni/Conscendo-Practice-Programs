a44fb5c13c23d7ca7d977b41dc5ec21cc31e393e
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-27 18:12:05 +0530

    not this.data in line 11 of js it should be just data

--- force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
+++ force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
@@ -8,7 +8,7 @@ export default class ClientToServerCommunicationCmp extends LightningElement {
    @wire(getContacts) contacts;
     @wire(getContacts) contactsFunction({error,data}){
         if(data){
-            this.listofContacts = this.data;
+            this.listofContacts = data;
         }
         else if(error){
             console.error('Error:'+error);
