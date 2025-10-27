a1cc8cb6561474cbae603348dfbb45468b0eef2a
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-27 18:38:42 +0530

    Practising the same component

--- force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
+++ force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
@@ -3,15 +3,15 @@ import getPersonName from '@salesforce/apex/SampleApexLtngWebCmpnt.getPersonName
 import getContacts from '@salesforce/apex/SampleApexLtngWebCmpnt.getContacts';
 
 export default class ClientToServerCommunicationCmp extends LightningElement {
-    listofContacts;
    @wire(getPersonName) personName;
+   listofContacts;
    @wire(getContacts) contacts;
-    @wire(getContacts) contactsFunction({error,data}){
-        if(data){
-            this.listofContacts = data;
-        }
-        else if(error){
-            console.error('Error:'+error);
-        }
+   @wire(getContacts) contactsFunction({error,data}){
+    if(data){
+        this.listofContacts = data;
     }
+    else if(error){
+        console.error('Error:'+error);
+    }
+   }
 }
