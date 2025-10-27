200933c652d4ed2238b429d9a371fa3d70303ad2
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-27 11:30:08 +0530

    DataBindingWebCmp added

---
+++ force-app/main/default/lwc/dataBindingWebCmp/dataBindingWebCmp.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class DataBindingWebCmp extends LightningElement {
+    Name='Pratap Sinha';
+    Age='60';
+    Address;
+    Businesses = ['Textiles','Chemicals','Education','Hardware'];
+    isShow = false;
+    dynamicVar;
+    getAddress(event){
+        this.Address = 'Noida';
+        this.Businesses.push('Education');
+        this.isShow=true;
+
+    }
+    getLiveData(event){
+        this.dynamicVar = event.target.value;
+    }
+}
