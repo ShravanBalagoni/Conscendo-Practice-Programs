50ff658e9712ef70eb65b5b242889aef6225fa2c
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-27 14:45:19 +0530

    @auraEnabled(cacheable =true) is missed

--- force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
+++ force-app/main/default/lwc/clientToServerCommunicationCmp/clientToServerCommunicationCmp.js
@@ -2,6 +2,7 @@ import { LightningElement,wire } from 'lwc';
 import getPersonName from '@salesforce/apex/SampleApexLtngWebCmpnt.getPersonName';
 import getContacts from '@salesforce/apex/SampleApexLtngWebCmpnt.getContacts';
 
+
 export default class ClientToServerCommunicationCmp extends LightningElement {
     @wire (getPersonName) personName;
     listOfContacts;
