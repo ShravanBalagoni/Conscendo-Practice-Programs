b217a8de81430df747a359557aeb3594b3ebd369
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-28 15:41:05 +0530

    createAccountLWC js  contents are added

--- force-app/main/default/lwc/createAccountLWC/createAccountLWC.js
+++ force-app/main/default/lwc/createAccountLWC/createAccountLWC.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
-
-export default class CreateAccountLWC extends LightningElement {}
+import doCreateAccount from '@salesforce/apex/SampleApexLtngWebCmpnt.doCreateAccount';
+export default class CreateAccountLWC extends LightningElement {
+    accountNameVar;
+    message;
+    handleinput(event){
+        this.accountNameVar = event.target.value;
+    }
+    createAccount(event){
+        doCreateAccount({acntName:this.accountNameVar}).then(result=>{
+            this.message = 'Account Inserted:'+result;
+        }).catch(error=>{
+            this.message = 'Account Not Inserted:'+error.body.message;
+        })
+    }
+}
