35b685853f82a55e6c104d84ee3f181febbf2f35
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-28 15:15:38 +0530

    js reimplemented on own

--- force-app/main/default/lwc/clientToServerCommunicationImperative/clientToServerCommunicationImperative.js
+++ force-app/main/default/lwc/clientToServerCommunicationImperative/clientToServerCommunicationImperative.js
@@ -1,12 +1,11 @@
 import { LightningElement } from 'lwc';
 import getAccountIMP from '@salesforce/apex/SampleApexLtngWebCmpnt.getAccountIMP';
-const columns = [
-    { label: 'Account Name', fieldName: 'Name' },
-    { label: 'Account Industry', fieldName: 'Industry' },
-    { label: 'Account Phone', fieldName: 'Phone' },
-    { label: 'Account Rating', fieldName: 'Rating' }
+const columns = [{label:'Account Name',fieldName:'Name'},
+                 {label:'Account Phone',fieldName:'Phone'},
+                 {label:'Account Rating',fieldName:'Rating'},
+                 {label:'Account Industry',fieldName:'Industry'},
+                 {label:'Account Fax',fieldName:'Fax'}
 ];
-
 export default class ClientToServerCommunicationImperative extends LightningElement {
     accounts;
     columnsList = columns;
@@ -15,7 +14,7 @@ export default class ClientToServerCommunicationImperative extends LightningElem
             this.accounts = result;
             console.log(result);
         }).catch(error=>{
-            console.log('Error'+error);
-        });
+            console.log('ERROR:',error);
+        })
     }
 }
