4b90d16eaa9f0c4457e0f9b8b746cb2f9f7ae3eb
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-28 14:57:16 +0530

    ClientToServerCommunicationImperative js component is added

--- force-app/main/default/lwc/clientToServerCommunicationImperative/clientToServerCommunicationImperative.js
+++ force-app/main/default/lwc/clientToServerCommunicationImperative/clientToServerCommunicationImperative.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
+import getAccountIMP from '@salesforce/apex/SampleApexLtngWebCmpnt.getAccountIMP';
+const columns = [
+    { label: 'Account Name', fieldName: 'Name' },
+    { label: 'Account Industry', fieldName: 'Industry' },
+    { label: 'Account Phone', fieldName: 'Phone' },
+    { label: 'Account Rating', fieldName: 'Rating' }
+];
 
-export default class ClientToServerCommunicationImperative extends LightningElement {}
+export default class ClientToServerCommunicationImperative extends LightningElement {
+    accounts;
+    columnsList = columns;
+    fetchAllAccounts(event){
+        getAccountIMP().then(result=>{
+            this.accounts = result;
+            console.log(result);
+        }).catch(error=>{
+            console.log('Error'+error);
+        });
+    }
+}
