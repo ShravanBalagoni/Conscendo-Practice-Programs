3517bab824dddfc1034f9f99b8dec3116bece628
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-28 10:23:09 +0530

    showAllRelatedContacts js is added

--- force-app/main/default/lwc/showAllRelatedContacts/showAllRelatedContacts.js
+++ force-app/main/default/lwc/showAllRelatedContacts/showAllRelatedContacts.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire,api } from 'lwc';
+import getrelatedContacts from '@salesforce/apex/relatedContactsApex.getrelatedContacts';
 
-export default class ShowAllRelatedContacts extends LightningElement {}
+export default class ShowAllRelatedContacts extends LightningElement {
+    @api recordId;
+    relContacts;
+    @wire(getrelatedContacts,{accountId:'$recordId'})
+    wiredData({error,data}){
+        if(data){
+            console.log('DATA:',data);
+            this.relContacts = data;
+        }
+        else if(error){
+            console.error('Error:',error);
+        }
+    }
+
+
+}
