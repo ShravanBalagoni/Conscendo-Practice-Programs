b992de66eb67cb5e5d8f6dcde3ab354f7a3df495
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-30 15:57:26 +0530

    saved html file

--- force-app/main/default/lwc/smb_account_view_formLDS/smb_account_view_formLDS.js
+++ force-app/main/default/lwc/smb_account_view_formLDS/smb_account_view_formLDS.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class Smb_account_view_formLDS extends LightningElement {}
+export default class Smb_account_view_formLDS extends LightningElement {
+    @api recordId;
+}
