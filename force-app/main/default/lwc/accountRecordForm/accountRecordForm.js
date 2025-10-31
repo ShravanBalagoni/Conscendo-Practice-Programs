8311e717b2678982fc061f4603f9aab71cb4bc30
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-10-31 15:08:38 +0530

    reimplemented

--- force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
+++ force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
@@ -1,12 +1,12 @@
 import { LightningElement,api } from 'lwc';
-import ACCOUNT_OBJ from '@salesforce/schema/Account';
-import ACC_NAME from '@salesforce/schema/Account.Name';
-import ACC_PHONE from '@salesforce/schema/Account.Phone';
-import ACC_RATING from '@salesforce/schema/Account.Rating';
+import ACCOUNT_OBJ from'@salesforce/schema/Account';
+import Acc_Name from '@salesforce/schema/Account.Name';
+import Acc_Phone from '@salesforce/schema/Account.Phone';
+import Acc_Rating from '@salesforce/schema/Account.Rating';
+import Acc_Industry from '@salesforce/schema/Account.Industry';
+
 
 
 export default class AccountRecordForm extends LightningElement {
-    @api recordId;
-    accountObject = ACCOUNT_OBJ;
-    specificFields = [ACC_NAME,ACC_PHONE,ACC_RATING];
+    
 }
