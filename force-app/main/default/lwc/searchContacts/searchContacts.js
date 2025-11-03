49353233ad49b12b4d6b7df5333702c4e265df84
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-03 14:51:04 +0530

    html added

--- force-app/main/default/lwc/searchContacts/searchContacts.js
+++ force-app/main/default/lwc/searchContacts/searchContacts.js
@@ -1,5 +1,9 @@
 import { LightningElement,wire,track } from 'lwc';
-
+import getContacts from '@salesforce/apex/SampleApexLtngWebCmpnt.getContacts';
 export default class SearchContacts extends LightningElement {
-    
+    @track searchKey = '';
+    @wire(getContacts,{searchKey:'$searchKey'})
+    contacts;
+    han
+
 }
