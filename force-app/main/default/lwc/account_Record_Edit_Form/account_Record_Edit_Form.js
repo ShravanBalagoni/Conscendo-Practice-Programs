def21c86ede384a4f17d8a3b3522edb9a80a8374
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-03 14:34:12 +0530

    preventDefault not preventDefaults

--- force-app/main/default/lwc/account_Record_Edit_Form/account_Record_Edit_Form.js
+++ force-app/main/default/lwc/account_Record_Edit_Form/account_Record_Edit_Form.js
@@ -4,7 +4,7 @@ export default class Account_Record_Edit_Form extends LightningElement {
     @api recordId;
     handleSubmitAction(event){
         console.log('--1--onsubmit--');
-        event.preventDefaults();
+        event.preventDefault();
         const fields = event.detail.fields;
         fields.Name = fields.Name+'Shravan LDS Update';
         this.template.querySelector('lightning-record-edit-form').submit(fields);
