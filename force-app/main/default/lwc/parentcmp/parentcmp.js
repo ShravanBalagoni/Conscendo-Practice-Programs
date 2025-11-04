f74a0411693deec40772a28b3199a562d655fe8a
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-04 15:33:05 +0530

    parentcmp js is added

--- force-app/main/default/lwc/parentcmp/parentcmp.js
+++ force-app/main/default/lwc/parentcmp/parentcmp.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class Parentcmp extends LightningElement {}
+export default class Parentcmp extends LightningElement {
+    someparentvar = 'this msg cmg from the Parent Lwc Cmp ';
+    ParentReceiver = null;
+    handleeventaction(event){
+        const info = event.detail;
+        this.ParentReceiver = info.message;
+    }
+}
