7149899aeb402fb33af0908f6aec33707a3a6acc
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-05 11:14:07 +0530

    removed to reimplement

--- force-app/main/default/lwc/childcmp/childcmp.js
+++ force-app/main/default/lwc/childcmp/childcmp.js
@@ -1,11 +1,6 @@
 import { LightningElement,api } from 'lwc';
 
 export default class Childcmp extends LightningElement {
-    @api childreceiverVar='';
-    passToparent ='Hello this msg is from child to parent ';
-    fireEventAction(event){
-        const custEvent = new CustomEvent('childeventname',{detail:{message:this.passToparent}})
-         this.dispatchEvent(custEvent);
-    }
+   
 
 }   
--- force-app/main/default/lwc/parentcmp/parentcmp.js
+++ force-app/main/default/lwc/parentcmp/parentcmp.js
@@ -1,10 +1,5 @@
 import { LightningElement } from 'lwc';
 
 export default class Parentcmp extends LightningElement {
-    someparentvar = 'this msg cmg from the Parent Lwc Cmp ';
-    ParentReceiver = null;
-    handleeventaction(event){
-        const info = event.detail;
-        this.ParentReceiver = info.message;
-    }
+    
 }
