f09ff28d9ca1afd6217fc77bd3b5ec9e2ef034ee
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-05 12:09:55 +0530

    commit

--- force-app/main/default/lwc/childcmp/childcmp.js
+++ force-app/main/default/lwc/childcmp/childcmp.js
@@ -1,7 +1,7 @@
 import { LightningElement, api } from 'lwc';
 
 export default class Childcmp extends LightningElement {
- @api childreceiverVar='';
+ @api childReceiverVar='';
  passToparent = 'Hello this is msg cmg from child';
  fireEventAction(event){
     const cusEvent = new CustomEvent('childevent',{detail:{message:this.passToparent}})
