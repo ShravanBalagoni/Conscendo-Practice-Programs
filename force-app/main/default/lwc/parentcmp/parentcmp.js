a8624e874da2c52ac0834cd60ba7b1244ff98553
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-05 11:58:55 +0530

    doing changes to js as well

--- force-app/main/default/lwc/parentcmp/parentcmp.js
+++ force-app/main/default/lwc/parentcmp/parentcmp.js
@@ -1,5 +1,6 @@
 import { LightningElement } from 'lwc';
 
 export default class Parentcmp extends LightningElement {
- 
+ someparentVar = 'this msg is coming from the parent';
+ parentReceiver='';
 }
