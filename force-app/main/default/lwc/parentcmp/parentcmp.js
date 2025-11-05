98454c31c6c41e154cf5d99bc39c6c26d01a1204
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-05 11:46:11 +0530

    modify

--- force-app/main/default/lwc/parentcmp/parentcmp.js
+++ force-app/main/default/lwc/parentcmp/parentcmp.js
@@ -1,10 +1,11 @@
 import { LightningElement } from 'lwc';
 
 export default class Parentcmp extends LightningElement {
-    someparentVar = 'this msg cmg from the parent Lwc cmp';
-    parentReceiver = null;
-    handleeventaction(event){
-        const info = event.detail;
-        this.parentReceiver = info.message;
-    }
+  someParentVar = 'this msg cmg from the parent Lwc cmp';
+  parentReceiver = '';
+
+  handleEventAction(event) {
+    const info = event.detail;
+    this.parentReceiver = info.message;
+  }
 }
