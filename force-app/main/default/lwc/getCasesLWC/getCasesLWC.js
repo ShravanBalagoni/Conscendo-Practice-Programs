90dc77f5a7eeca76d8098dfaed8c5b84ea59a9bb
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-06 17:49:23 +0530

    js added

--- force-app/main/default/lwc/getCasesLWC/getCasesLWC.js
+++ force-app/main/default/lwc/getCasesLWC/getCasesLWC.js
@@ -2,8 +2,16 @@ import { LightningElement } from 'lwc';
 import getCases from '@salesforce/apex/SampleApexLtngWebCmpnt.getCases';
 const columns = [{label:'CaseNo', fieldName:'CaseNumber'},
                 {label:'CasePriority', fieldName:'Priority'},
-                {label:'CaseOrigin', fieldName:'Origin'},
-];
+                {label:'CaseOrigin', fieldName:'Origin'}];
 export default class GetCasesLWC extends LightningElement {
+    cases;
+    columns=columnsList;
+    fetchAllCases(event){
+        getCases().then(result=>{
+            this.cases = result;
+        }).catch(error=>{
+            console.error('ERROR:'+error);
+        })
+    }
 
 }
