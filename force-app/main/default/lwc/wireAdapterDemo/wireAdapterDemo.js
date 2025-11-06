5c8c94e156f6f72199b8535281419c0288a61e4e
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-06 11:21:17 +0530

    used the object fields using string

--- force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
+++ force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
@@ -1,15 +1,15 @@
 import { LightningElement,wire,api } from 'lwc';
 import { getRecord,getFieldValue } from 'lightning/uiRecordApi';
-import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
-import ACCOUNT_PHONE from '@salesforce/schema/Account.Phone';
+//import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
+//import ACCOUNT_PHONE from '@salesforce/schema/Account.Phone';
 export default class WireAdapterDemo extends LightningElement {
   @api recordId;
-  @wire(getRecord,{recordId:'$recordId',fields:[ACCOUNT_NAME,ACCOUNT_PHONE]}) record;
+  @wire(getRecord,{recordId:'$recordId',fields:['Account.Name','Account.Phone']}) record;
 
   get name(){
-    return this.record.data?getFieldValue(this.record.data,ACCOUNT_NAME):'';
+    return this.record.data?getFieldValue(this.record.data,'Account.Name'):'';
   }
   get phone(){
-    return this.record.data?getFieldValue(this.record.data,ACCOUNT_PHONE):'';
+    return this.record.data?getFieldValue(this.record.data,'Account.Phone'):'';
   }
 }
