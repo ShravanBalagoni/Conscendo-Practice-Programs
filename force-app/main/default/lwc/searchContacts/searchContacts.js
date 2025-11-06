cfcb8a5e726422fbbc5c915ce18545cedaa7e99d
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-06 12:16:39 +0530

    imported search contacts apex in js

--- force-app/main/default/lwc/searchContacts/searchContacts.js
+++ force-app/main/default/lwc/searchContacts/searchContacts.js
@@ -1,5 +1,5 @@
 import { LightningElement,wire,track } from 'lwc';
-import getContacts from '@salesforce/apex/SampleApexLtngWebCmpnt.getContacts';
+import getContacts from '@salesforce/apex/searchContacts.getContacts';
 export default class SearchContacts extends LightningElement {
     @track searchKey = '';
     @wire(getContacts,{searchKey:'$searchKey'})
