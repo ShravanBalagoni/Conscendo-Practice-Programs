51e2452ba352249e2a58b560d89e8e25c789b971
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-06 11:39:32 +0530

    corrected else to else if in line 13 of showOpp js

--- force-app/main/default/lwc/showOpportunities/showOpportunities.js
+++ force-app/main/default/lwc/showOpportunities/showOpportunities.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,wire} from 'lwc';
+import getRelatedOpps from '@salesforce/apex/SampleApexLtngWebCmpnt.getRelatedOpps';
 
-export default class ShowOpportunities extends LightningElement {}
+export default class ShowOpportunities extends LightningElement {
+    @api recordId;
+    opps;
+    @wire(getRelatedOpps,{accIdVar:'$recordId'}) 
+    records({error,data}){
+        if(data){
+            this.opps=data;
+            console.log(this.opps);
+        }
+        else if(error){
+            console.error('Error:'+error);
+        }
+    }
+}
