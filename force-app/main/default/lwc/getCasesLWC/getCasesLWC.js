8375a5c71b6dbb13381858d64c838b0d157bfb87
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-06 17:46:29 +0530

    added origin in apex class

--- force-app/main/default/lwc/getCasesLWC/getCasesLWC.js
+++ force-app/main/default/lwc/getCasesLWC/getCasesLWC.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
+import getCases from '@salesforce/apex/SampleApexLtngWebCmpnt.getCases';
+const columns = [{label:'CaseNo', fieldName:'CaseNumber'},
+                {label:'CasePriority', fieldName:'Priority'},
+                {label:'CaseOrigin', fieldName:'Origin'},
+];
+export default class GetCasesLWC extends LightningElement {
 
-export default class GetCasesLWC extends LightningElement {}
+}
