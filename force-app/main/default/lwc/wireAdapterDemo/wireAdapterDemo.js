7fb1efe8402ea80157fc5e9a5951290cadc714a3
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-06 10:21:10 +0530

    wireadaptor js component added

--- force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
+++ force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
@@ -1,3 +1,16 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement,wire,api } from 'lwc';
+import { getRecord,getFieldValue } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import PHONE_FIELD from '@salesforce/schema/Account.Phone';
+export default class WireAdapterDemo extends LightningElement {
+    @api recordId;
+    @wire(getRecord,{recordId:'$recordId',fields:[NAME_FIELD,PHONE_FIELD]})
+    record;
 
-export default class WireAdapterDemo extends LightningElement {}
+    get name(){
+        return this.record.data ? getFieldValue(this.record.data,NAME_FIELD):'';
+    }
+    get phone(){
+        return this.record.data ? getFieldValue(this.record.data.PHONE_FIELD):'';
+    }
+}
