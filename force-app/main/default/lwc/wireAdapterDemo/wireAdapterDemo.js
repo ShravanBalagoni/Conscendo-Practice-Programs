8e3cc6685be9c327b83e9bc132cbe74c6ad3519e
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-06 11:14:31 +0530

    REIMPLEMENTED JS

--- force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
+++ force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
@@ -1,16 +1,15 @@
 import { LightningElement,wire,api } from 'lwc';
 import { getRecord,getFieldValue } from 'lightning/uiRecordApi';
-import NAME_FIELD from '@salesforce/schema/Account.Name';
-import PHONE_FIELD from '@salesforce/schema/Account.Phone';
+import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
+import ACCOUNT_PHONE from '@salesforce/schema/Account.Phone';
 export default class WireAdapterDemo extends LightningElement {
-    @api recordId;
-    @wire(getRecord,{recordId:'$recordId',fields:[NAME_FIELD,PHONE_FIELD]})
-    record;
+  @api recordId;
+  @wire(getRecord,{recordId:'$recordId',fields:[ACCOUNT_NAME,ACCOUNT_PHONE]}) record;
 
-    get name(){
-        return this.record.data ? getFieldValue(this.record.data,NAME_FIELD):'';
-    }
-    get phone(){
-        return this.record.data ? getFieldValue(this.record.data,PHONE_FIELD):'';
-    }
+  get name(){
+    return this.record.data?getFieldValue(this.record.data,ACCOUNT_NAME):'';
+  }
+  get phone(){
+    return this.record.data?getFieldValue(this.record.data,ACCOUNT_PHONE):'';
+  }
 }
