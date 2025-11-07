c91d5559f0e22aee89b05932ec0dedc4abd306cc
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-07 14:51:04 +0530

    reimplement

--- force-app/main/default/lwc/contactChildLWCCmp/contactChildLWCCmp.js
+++ force-app/main/default/lwc/contactChildLWCCmp/contactChildLWCCmp.js
@@ -1,39 +1,23 @@
 import { LightningElement ,api} from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 export default class ContactChildLWCCmp extends LightningElement {
-    @api accountId;
+    @api recordId;
     lastName = null;
+    
     phone=null;
-    birthdate=null;
-    postalcode=null;
+    birthDate=null;
+    postalCode=null;
     captureData(event){
-        const inputName = event.target.name;
+        const inputName=event.target.name;
         if(inputName==='lname'){
-            this.lastName = event.target.value;
-        }else  if(inputName==='phone'){
-            this.phone = event.target.value;
+            this.lastName=event.target.value;
+        } else if(inputName==='phone'){
+            this.phone=event.target.value;
         }else if(inputName==='birthdate'){
-            this.birthdate = event.target.value;
+            this.birthDate=event.target.value;
         }else if(inputName==='PostalCode'){
-    this.postalcode = event.target.value;
-}
-
+            this.postalCode=event.target.value;
+        }
     }
-    saveRecord(event){
-        console.log('Acnt id:'+this.accountId);
-        const fields={
-            'LastName':this.lastName,
-            'Phone':this.phone,
-            'Birthdate':this.birthdate,
-            'MailingPostalCode':this.postalcode,
-            'AccountId':this.accountId
-        };
-        const recordData = {apiName:'Contact',fields};
-        createRecord(recordData).then(response => {
-            alert('Record saved...!'+response.id);
-            
-        }).catch(error => {
-           alert('Record failed...!'+error.body.message);
-        });
-    }
+    
 }
