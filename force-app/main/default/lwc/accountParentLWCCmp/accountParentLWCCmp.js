11ccfce1d590eb28216bf0e67b3bb9f0a6681328
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-07 14:23:16 +0530

    re implement

--- force-app/main/default/lwc/accountParentLWCCmp/accountParentLWCCmp.js
+++ force-app/main/default/lwc/accountParentLWCCmp/accountParentLWCCmp.js
@@ -2,43 +2,16 @@ import { LightningElement } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 
 export default class AccountParentLWCCmp extends LightningElement {
-    accountrecordid = null;
-    nameVar = null;
-    accountNumber = null;
-    showchildcmp = false;
-
-    collectInputs(event) {
-        const inputName = event.target.name;
-        if (inputName === 'AccountName') {
-            this.nameVar = event.target.value;
-        } else if (inputName === 'AccountNumber') {
-            this.accountNumber = event.target.value;
-        }
+  accountrecordid=null;
+  nameVar=null;
+  accountNumber=null;
+  showchildcmp=false;
+  collectInputs(event){
+    const inputName = event.target.name;
+    if(inputName==='AccountName'){
+        this.nameVar=event.target.value;
+    } else if(inputName==='AccountNumber'){
+        this.accountNumber=event.target.value;
     }
-
-    saveAccountRecords() {
-        console.log('The Process of button is activated');
-
-        const fields = {
-            Name: this.nameVar,
-            AccountNumber: this.accountNumber
-        };
-
-        const inputRecord = { apiName: 'Account', fields };
-        createRecord(inputRecord)
-            .then((response) => {
-                this.accountrecordid = response.id;
-                this.showchildcmp = true;
-            })
-            .catch((error) => {
-                console.error('Error creating account:', error);
-                // In LWC, avoid alert in production; keeping here to match current behavior
-                // Ensure error.body and message exist before accessing
-                const msg =
-                    (error && error.body && error.body.message) ||
-                    (error && error.message) ||
-                    'Unknown error';
-                alert('Error creating account: ' + msg);
-            });
-    }
+  }
 }
