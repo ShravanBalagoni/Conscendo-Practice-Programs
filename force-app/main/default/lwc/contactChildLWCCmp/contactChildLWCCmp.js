040bd0fae1868496903d792546070be7e5ef699f
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-07 15:01:40 +0530

    re-implement

--- force-app/main/default/lwc/contactChildLWCCmp/contactChildLWCCmp.js
+++ force-app/main/default/lwc/contactChildLWCCmp/contactChildLWCCmp.js
@@ -20,7 +20,20 @@ export default class ContactChildLWCCmp extends LightningElement {
         }
     }
     saveRecord(event){
-        console.log('Account Id:')
+        console.log('Account Id:',this.accountId);
+        const fields={
+            'LastName':this.lastName,
+            'Phone':this.phone,
+            'Birthdate':this.birthDate,
+            'MailingPostalCode':this.postalCode,
+            'AccountId':this.accountId
+        }
+        const inputRecord={apiName:'Contact',fields};
+        createRecord(inputRecord).then(response=>{
+            alert('Record Saved:'+response.id);
+        }).catch(error=>{
+            alert('Record Not Saved:+error.body.message');
+        })
     }
 
 }
