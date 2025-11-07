58f023903a976f38954b8c4f518a22ed1f0a944c
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-07 11:34:44 +0530

    footer added

--- force-app/main/default/lwc/accountParentLWCCmp/accountParentLWCCmp.js
+++ force-app/main/default/lwc/accountParentLWCCmp/accountParentLWCCmp.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
 
-export default class AccountParentLWCCmp extends LightningElement {}
+export default class AccountParentLWCCmp extends LightningElement {
+    accountrecordid=null;
+    nameVar=null;
+    accountNumber=null;
+    showchildcmp=false;
+}
