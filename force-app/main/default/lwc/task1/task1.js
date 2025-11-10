063e604bb3e44f972a559790fc4b268163c757c1
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-10 10:48:38 +0530

    js added

--- force-app/main/default/lwc/task1/task1.js
+++ force-app/main/default/lwc/task1/task1.js
@@ -1,29 +1,30 @@
 import { LightningElement } from 'lwc';
-import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
-import LAST_NAME from '@salesforce/schema/Contact.LastName';
-import  EMAIL from '@salesforce/schema/Contact.Email';
-import PHONE from '@salesforce/schema/Contact.Phone';
+
 export default class Task1 extends LightningElement {
+    firstName='';
+    lastName='';
+    emaill;
+    phoneno='';
     collectInputs(event){
         const inputName=event.target.name;
         if(inputName==='FName'){
-            this.FIRST_NAME = event.target.value;
+            this.firstName = event.target.value;
             //console.log(event.target.value);
         }else if(inputName==='LName'){
-            this.LAST_NAME = event.target.value;
+            this.lastName = event.target.value;
            // console.log(event.target.value);
         }else if(inputName==='email'){
-            this.EMAIL = event.target.value;
+            this.emaill = event.target.value;
            // console.log(event.target.value);
         }else if(inputName==='phone'){
-            this.PHONE = event.target.value;
+            this.phoneno = event.target.value;
            // console.log(event.target.value);
         }
     }
     handleClick(event){
-        console.log('First Name:'+FIRST_NAME);
-        console.log('Last Name:'+LAST_NAME);
-        console.log('Email:'+EMAIL);
-        console.log('Phone:'+PHONE);
+        console.log('First Name:'+firstName);
+        console.log('Last Name:'+lastName);
+        console.log('Email:'+emaill);
+        console.log('Phone:'+phoneno);
     }
 }
