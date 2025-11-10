129ec5b32219fa398801c51ab7b6f145053cd5f9
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-10 09:58:40 +0530

    added slds grid for input tags

--- force-app/main/default/lwc/task1/task1.js
+++ force-app/main/default/lwc/task1/task1.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
-
-export default class Task1 extends LightningElement {}
+import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
+import LAST_NAME from '@salesforce/schema/Contact.LastName';
+import  EMAIL from '@salesforce/schema/Contact.Email';
+import PHONE from '@salesforce/schema/Contact.Phone';
+export default class Task1 extends LightningElement {
+    
+}
