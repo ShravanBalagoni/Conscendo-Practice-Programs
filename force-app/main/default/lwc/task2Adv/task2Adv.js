e6f5eba81ebe260aac5d300282350593246367f9
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-11 15:43:33 +0530

    AccountController Modified

--- force-app/main/default/lwc/task2Adv/task2Adv.js
+++ force-app/main/default/lwc/task2Adv/task2Adv.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
+import {showToastEvent} from 'lightning/platformShowToastEvent';
+import insertAccount from '@salesforce/apex/AccountController.insertAccount';
+import updateAccount from '@salesforce/apex/AccountController.updateAccount';
+export default class Task2Adv extends LightningElement {
 
-export default class Task2Adv extends LightningElement {}
+}
