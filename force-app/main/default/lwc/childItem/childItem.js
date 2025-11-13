21961a4863b1de4716b1692db7b01f584a839706
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-13 11:03:15 +0530

    js component is added

--- force-app/main/default/lwc/childItem/childItem.js
+++ force-app/main/default/lwc/childItem/childItem.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class ChildItem extends LightningElement {}
+
+export default class ChildItem extends LightningElement {
+@api itemId; // id passed from parent
+@track selected = false;
+
+
+// expose a reset method so parent / grandparent can call it
+@api
+reset() {
+this.selected = false;
+// Also notify upward that this child is now deselected
+this._dispatchToggleEvent();
+}
+
+
+get buttonLabel() {
+return this.selected ? 'Deselect' : 'Select';
+}
+
+
+get statusLabel() {
+return this.selected ? 'Selected' : 'Deselected';
+}
+
+
+toggleSelect() {
+this.selected = !this.selected;
+this._dispatchToggleEvent();
+}
+
+
+_dispatchToggleEvent() {
+// Custom event that bubbles and is composed so it crosses shadow boundaries
+const detail = { id: this.itemId, selected: this.selected };
+const evt = new CustomEvent('childtoggle', {
+detail,
+bubbles: true,
+composed: true,
+});
+this.dispatchEvent(evt);
+}
+}
