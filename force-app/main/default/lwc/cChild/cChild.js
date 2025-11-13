3a6480210c18f6be5a897c1e71e1c61aca6c1421
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-13 10:25:34 +0530

    cChild.js added

--- force-app/main/default/lwc/cChild/cChild.js
+++ force-app/main/default/lwc/cChild/cChild.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class CChild extends LightningElement {}
+export default class CChild extends LightningElement {
+  @track isSelected = false;
+
+  get buttonLabel() {
+    return this.isSelected ? 'Deselect' : 'Select';
+  }
+
+  toggleSelect() {
+    this.isSelected = !this.isSelected;
+    this.dispatchEvent(new CustomEvent('toggleselect', {
+      bubbles: true,
+      composed: true,
+      detail: { selected: this.isSelected }
+    }));
+  }
+
+  @api
+  resetSelection() {
+    this.isSelected = false;
+  }
+}
