55e19c0cb721652a22ad5405338425216413c1c3
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-17 16:09:22 +0530

    Remove unnecessary whitespace in childItem.js for cleaner code

--- force-app/main/default/lwc/childItem/childItem.js
+++ force-app/main/default/lwc/childItem/childItem.js
@@ -1,11 +1,7 @@
 import { LightningElement, api, track } from 'lwc';
-
-
 export default class ChildItem extends LightningElement {
 @api itemId; // id passed from parent
 @track selected = false;
-
-
 // expose a reset method so parent / grandparent can call it
 @api
 reset() {
@@ -13,24 +9,16 @@ this.selected = false;
 // Also notify upward that this child is now deselected
 this._dispatchToggleEvent();
 }
-
-
 get buttonLabel() {
 return this.selected ? 'Deselect' : 'Select';
 }
-
-
 get statusLabel() {
 return this.selected ? 'Selected' : 'Deselected';
 }
-
-
 toggleSelect() {
 this.selected = !this.selected;
 this._dispatchToggleEvent();
 }
-
-
 _dispatchToggleEvent() {
 // Custom event that bubbles and is composed so it crosses shadow boundaries
 const detail = { id: this.itemId, selected: this.selected };
