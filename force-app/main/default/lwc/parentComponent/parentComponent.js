8818841173c2445ba99014f2babef239755b5d63
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-18 14:46:40 +0530

    Add content to parentComponent.html to display child component messages

--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,5 +1,10 @@
 import { LightningElement } from 'lwc';
 
 export default class ParentComponent extends LightningElement {
-    
+    parentMessage="this is the message from the parent to child"
+    parentReceiver='';
+    handleeventAction(event){
+        const info = event.detail;
+        this.parentReceiver= info.message;
+    }
 }
