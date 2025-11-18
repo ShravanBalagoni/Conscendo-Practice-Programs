cb7512b9689f01d124c4a494d114ec3eec6c8f8a
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-18 14:40:40 +0530

    Refactor ChildComponent to standardize message format and improve event dispatching

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,7 +1,10 @@
 import { LightningElement ,api} from 'lwc';
 
 export default class ChildComponent extends LightningElement {
-    @api childreceiverVar = '';
-    childmessage='This is the message from the child LWC'
-    Custom
+    @api childreceiverVar='';
+    childmessage="this is the message from child to parent";
+    fireEventAction(event){
+        const cusEvnt = new CustomEvent('childEvent',{detail:{message:this.childmessage}})
+        this.dispatchEvent(cusEvnt);
+    }
 }
