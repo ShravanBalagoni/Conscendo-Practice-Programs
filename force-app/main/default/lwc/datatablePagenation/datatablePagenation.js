44dc00dc7086aafd9117fcc2055f20d16384dd90
ShravanBalagoni <balagoni.shravan6@gmail.com>
2025-11-26 17:35:36 +0530

    feat(datatablePagenation): implement pagination for account data; enhance data retrieval and navigation functionality

--- force-app/main/default/lwc/datatablePagenation/datatablePagenation.js
+++ force-app/main/default/lwc/datatablePagenation/datatablePagenation.js
@@ -1,28 +1,51 @@
 import { LightningElement } from 'lwc';
 import getAccountIMP from '@salesforce/apex/SampleApexLtngWebCmpnt.getAccountIMP';
-/*
-import Name from '@salesforce/schema/Account.Name';
-import AccountNumber from '@salesforce/schema/Account.AccountNumber';
-import Phone from '@salesforce/schema/Account.Phone';
-import Industry from '@salesforce/schema/Account.Industry';
-import Rating from '@salesforce/schema/Account.Rating';*/
 
-const columns = [{label:'Account Name',fieldName:Name},
-                 {label:'Account Number',fieldName:AccountNumber},
-                 {label:'Phone Number',fieldName:Phone},
-                 {label:'Account Industry',fieldName:Industry},
-                 {label:'Account Rating',fieldName:Rating}
+const columns = [{label:'Account Name',fieldName:'Name'},
+                 {label:'Account Number',fieldName:'AccountNumber'},
+                 {label:'Phone Number',fieldName:'Phone'},
+                 {label:'Account Industry',fieldName:'Industry'},
+                 {label:'Account Rating',fieldName:'Rating'}
                 
 ];
 export default class DatatablePagenation extends LightningElement {
     columnsList = columns;
-    accounts;
+    accounts = [];
+    paginatedAccounts = [];
+    currentPage = 1;
+    pageSize = 10;
+    totalPages = 0;
     connectedCallback(){
     getAccountIMP().then(result=>{
         this.accounts = result;
+        this.totalPages = Math.ceil(this.accounts.length/this.pageSize);
+        this.updatedPaginatedAccounts();
 
     }).catch(error=>{
         console.log('Error:'+error);
     })
 }
+updatedPaginatedAccounts(){
+    const startIndex = (this.currentPage -1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedAccounts = this.accounts.slice(startIndex,endIndex);
+}
+get isPreviousDisabled(){
+    return this.currentPage === 1;
+}
+get isNextDisabled(){
+    return this.currentPage === this.totalPages;
+}
+handlePrevious(){
+    if(this.currentPage>1){
+        this.currentPage--;
+        this.updatedPaginatedAccounts();
+    }
+    }
+    handleNext(){
+        if(this.currentPage<this.totalPages){
+            this.currentPage++;
+            this.updatedPaginatedAccounts();
+        }
+    }
 }
