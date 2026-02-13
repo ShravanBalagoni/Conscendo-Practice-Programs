4171a1d3b67a3e9226ba964770dd8915a54ce3e9
ShravanBalagoni <balagoni.shravan6@gmail.com>
2026-02-13 15:58:46 +0530

    feat: Refactor pagination component to search Accounts instead of Contacts; update Apex method and JavaScript logic

--- force-app/main/default/lwc/paginationCmp/paginationCmp.js
+++ force-app/main/default/lwc/paginationCmp/paginationCmp.js
@@ -1,3 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getContactsPg from '@salesforce/apex/searchContacts.getContactsPg';
+export default class PaginationCmp extends LightningElement {
+    @track searchKey = '';
+    @track contacts =[];
+    @track pageNumber = 1;
 
-export default class PaginationCmp extends LightningElement {}
+    handleKeyChange(event){
+        this.searchKey = event.target.value;
+    }
+
+    handleSearch(){
+        this.pageNumber = 1;
+        this.searchPage();
+    }
+
+    searchPage(){
+        getContactsPg({searchKey:this.searchKey,pageNumber:this.pageNumber})
+        .then(result=>{
+            this.contacts = result.contacts;
+            console.log('Page', this.pageNumber,':',this.contacts);
+        })
+        .catch(error=>{
+            console.error('Error:',error);
+        });
+    }
+
+    goNext(){
+        this.pageNumber++;
+        this.searchPage();
+    }
+
+    goPrevious(){
+        if(this.pageNumber > 1){
+            this.pageNumber--;
+            this.searchPage();
+        }
+    }
+
+}
