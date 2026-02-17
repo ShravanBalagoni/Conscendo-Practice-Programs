3e097371b8e9616eb47e5869e25bfc77f28d841b
ShravanBalagoni <balagoni.shravan6@gmail.com>
2026-02-17 14:40:33 +0530

    feat: Add DecisionModal component; implement decision handling and user comment functionality

---
+++ force-app/main/default/lwc/decisionModal/decisionModal.js
@@ -0,0 +1,58 @@
+import { LightningElement } from 'lwc';
+import {api} from 'lwc';
+import createAccountDecision from '@salesforce/apex/AccountDecisionService.createAccountDecision';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+export default class DecisionModal extends LightningElement {
+    @api recordId;
+    @api recordLabel;
+
+    options = [
+        {id:'Approved', label:' Approved'},
+        {id:'Rejected',label:' Rejected'},
+        { id: 'Hold', label: '⏸ On Hold' },
+        { id: 'Escalated', label: ' Escalated' }
+    ];
+
+    userComment = '';
+    isLoading = false;
+
+    handleInputChange(event){
+        this.userComment = event.target.value;
+    }
+    handleOptionClick(event){
+        const decision = event.target.dataset.id;
+        this.submitDecision(decision);
+    }
+
+    async submitDecision(decision){
+        this.isLoading = true;
+
+        try{
+            await createAccountDecision({
+                accountId: this.recordId,
+                decision: decision,
+                comment: this.userComment
+            });
+
+            const result = {
+                success: true,
+                decision: decision,
+                comment: this.userComment
+            };
+            this.close(result);
+        }
+        catch(error){
+            this.dispatchEvent(new ShowToastEvent({
+                title:'Error',
+                message: error.body.message,
+                variant: 'error'
+            }));
+        } finally{
+            this.isLoading = false;
+        }
+    }
+    handleCancel(){
+        this.close(null);
+    }
+}
