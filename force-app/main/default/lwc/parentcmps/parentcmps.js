e0c0803527be7d8e562b50b936f1899aee3682a4
ShravanBalagoni <balagoni.shravan6@gmail.com>
2026-02-19 17:31:16 +0530

    feat: Implement Parentcmps component with message handling; add input, event firing, and message history display

--- force-app/main/default/lwc/parentcmps/parentcmps.js
+++ force-app/main/default/lwc/parentcmps/parentcmps.js
@@ -1,3 +1,46 @@
 import { LightningElement } from 'lwc';
 
-export default class Parentcmps extends LightningElement {}
+export default class Parentcmps extends LightningElement {
+    parentMessage = 'Hello Child!';
+
+    parentInput = '';
+    messageList = [];
+    totalMessages = 0;
+
+    handleParentInput(event){
+        this.parentInput = event.target.value;
+    }
+
+    sendToChild(){
+        if(!this.parentInput){
+            return;
+        }
+
+        this.parentMessage = this.parentInput;
+        this.parentInput = '';
+        this.template.querySelector('lightning-input').value = '';
+    }
+
+    handleeventAction(event){
+        const info = event.detail;
+
+        this.totalMessages++;
+
+        this.messageList = [
+            ...this.messageList,
+            {id: this.totalMessages, text: info.message}
+        ];
+    }
+    
+    handleChildReset(){
+        this.messageList = [];
+        this.totalMessages = 0;
+    }
+
+    resetEverything(){
+        this.messageList = [];
+        this.totalMessages = 0;
+        this.parentMessage = 'Hello Child!';
+        this.template.querySelector('c-childcmp').firstElementChild();
+    }
+}
